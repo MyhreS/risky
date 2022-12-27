5c3ca44befcda27792576d4b16b2e7240c90a763
Simon Myhre <simonmyhre1@gmail.com>
2022-12-27 14:17:09 +0100

    feat: correct test dice roll

--- tests/test_main_functions.py
+++ tests/test_main_functions.py
@@ -1,11 +1,12 @@
 import unittest
-from main import roll_dice, one_attack_defence_scenario
+from main import *
 
 
 class TestMainFunctions(unittest.TestCase):
     def test_roll_dice(self):
         dice_roll = roll_dice()
-        self.assertEqual(dice_roll in range(1, 7), True)
+        self.assertTrue(dice_roll in range(1, 7))
+
 
 
 if __name__ == '__main__':
