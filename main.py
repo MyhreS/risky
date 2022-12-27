d6bc64f7762d9205deb98e9bf368d71c4ecc8f09
Simon Myhre <simonmyhre1@gmail.com>
2022-12-27 14:31:33 +0100

    feat: add function for one attack defence scenario

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ import random
 def roll_dice():
     return random.randint(1, 6)
 
+def roll_attacker_and_defender(attacker, defender):
+    attacker_rolls = []
+    for i in range(attacker):
+        attacker_rolls.append(roll_dice())
+    attacker_rolls.sort(reverse=True)
+
+    defender_rolls = []
+    for i in range(defender):
+        defender_rolls.append(roll_dice())
+    defender_rolls.sort(reverse=True)
+    return attacker_rolls, defender_rolls
